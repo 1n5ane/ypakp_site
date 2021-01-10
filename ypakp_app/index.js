0ecf301c57b1370464eb3deb9cb031d24e9a5c35
in5ane <th.lolos1000@gmail.com>
2021-01-10 16:43:45 +0200

    First commit

---
+++ ypakp_app/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static(__dirname + '/'));
+
+app.get('/index.php', (req, res) => {
+  res.sendFile(__dirname+'/index.html');
+
+})
+
+app.get('/login.html', (req, res) => {
+  res.sendFile(__dirname+'/public/login.html');
+})
+
+app.get('/Register.html', (req, res) => {
+  res.sendFile(__dirname+'/public/Register.html');
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
